ddc0540c393789abd416f2efd40fb26bbe6d9272
Oakland Peters <oakland.peters@gmail.com>
2015-02-06 10:44:40 -0500

    TEMP Midway through refactoring. Running unittests during dev.

--- pyinterfaces/metatypes/pep484.py
+++ pyinterfaces/metatypes/pep484.py
@@ -5,8 +5,10 @@ it can just have Any/Union be instances
 
 @todo: Decide if Tuple and tuple should have an 'virtual' (abc.register) relationship.
 """
+import abc
 import types
 
+from ..valueabc.valueabc import ValueABC
 from .shared import TypeLogic
 
 __all__ = [
@@ -15,7 +17,8 @@ __all__ = [
     'Optional'
 ]
 
-class AnyType(object):
+
+class Any(ValueABC):
     """
     Back port of typing.Any from PEP 483, into pyinterfaces/ 2.7 implementation.
     This version of `Any` differs in some major ways. For example, this version
@@ -24,28 +27,212 @@ class AnyType(object):
         assert isinstance(typing.Any, SomeClass)
         assert not isinstance(metatypes.Any, SomeClass)
     """
-    def __init__(self, *args, **kwargs):
-        pass
-
+    @classmethod
     def __instancecheck__(self, instance):  # pylint: disable=unused-argument
         """
         @type: instance: Any
         @rtype: bool
         """
         return True
-
+    @classmethod
     def __subclasscheck__(self, subclass):  # pylint: disable=unused-argument
         """
         @type: subclass: type
         @rtype: bool
         """
+        if not isinstance(subclass, type):
+            raise TypeError(str.format(
+                "'subclass' must be a class."
+            ))
         return True
 
-# Instantiate Any
-Any = AnyType()  # pylint: disable=invalid-name
+
+class UnionMeta(type):
+    """
+    Metaclass for generating TypeUnion classes.
+
+    UnionMeta(...)
+    --> Union(...)
+    --> TypeUnion(...)
+
+    """
+    def __new__(mcls, name, bases, namespace):
+        """
+        Union(list, tuple) -->
+            TypeUnion = UnionMeta('TypeUnion', (,), {})
+            return TypeUnion
+
+        Triggers type.__new__, which will cause different behavior
+        for descendants, if mcls has __instancecheck__ and __subclasscheck__
+
+        Unconfirmed Belief: methods on mcls become classmethods on cls. Classmethods on mcls do not appear on cls at all.
+
+        @todo: Requires that namespaces includes 'types', and validate it.
+
+        @type: name: str
+        @type: bases: Sequence[type]
+        @type: namespace: dict[str, Any]
+        """
+
+        # Construct class - uses type.__new__
+        cls = super(UnionMeta, mcls).__new__(mcls, name, bases, namespace)
+
+        return cls
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Triggered whenever the newly created class is "called"
+        to instantiate a new object.
+
+        (mcls.__init__ controls the initialization of the new class,
+         NOT the initialization of the instances of the new class)
+        
+        I need to set Union(list, tuple) to return a class
+        ... this can be done either here in UnionMeta.__call__, or in Union.__new__
+        """
+        return cls.__new__(cls, *args, **kwargs)
 
 
-class Union(TypeLogic):
+    def __instancecheck__(cls, instance):  # pylint: disable=unused-argument
+        """
+        @type: instance: Any
+        @rtype: bool
+        """
+        if _hasattr(cls, "__instancecheck__"):
+            return cls.__instancecheck__(instance)
+        return NotImplemented
+        
+        # return any(
+        #     isinstance(instance, _type)
+        #     for _type in cls._types
+        # )
+
+
+    def __subclasscheck__(cls, subclass):  # pylint: disable=unused-argument
+        """
+        @type: subclass: type
+        @rtype: bool
+        """        
+        if _hasattr(cls, "__subclasscheck__"):
+            return cls.__subclasscheck__(subclass)
+        return NotImplemented
+
+
+
+class Union(object):
+    __metaclass__ = UnionMeta
+    def __new__(cls, *_types):
+        class TypeUnion(Union):
+            @classmethod
+            def __instancecheck__(cls, instance):                
+                return any(
+                    isinstance(instance, _type)
+                    for _type in cls._types
+                )
+            @classmethod
+            def __subclasscheck__(cls, subclass):
+                return any(
+                    issubclass(subclass, _type)
+                    for _type in cls._types
+                )
+        if len(_types) == 0:
+            _types = tuple([Any])
+        TypeUnion._types = _types  # pylint: disable=W0212
+        return TypeUnion
+
+
+
+# class Union(ValueABC):
+#     """
+#     ... trying to do this without a special metaclass
+
+#     @todo: Create with metaclass, defining
+#     __instancecheck__
+#     __repr__
+#     __subclasscheck__
+#     """
+#     _types = tuple()
+
+#     def __new__(cls, *_types):
+#         """
+#         Create new class, inheriting from this one, and return it.
+#         @todo: type-check _types
+
+#         @type: _types: Sequence[type]
+#         @rtype: Union
+#         """
+#         name = "UnionedTypes"
+#         bases = tuple([Union])
+
+#         def union_repr(cls):
+#             return repr(cls).replace(
+#                 "'>", "': "+repr(cls._types)+">"
+#             )
+#         namespace = {
+#             '_types': _types,
+#             '__repr__': classmethod(union_repr)
+#             # '__instancecheck__': Union.__instancecheck__,
+#             # '__subclasscheck__': Union.__subclasscheck__
+#         }
+#         return type(name, bases, namespace)
+
+
+#     @classmethod
+#     def __instancecheck__(cls, instance):  # pylint: disable=unused-argument
+#         """
+#         @type: instance: Any
+#         @rtype: bool
+#         """
+#         # ... prevent recursion
+#         if isinstance(instance, type):
+#             if Union in instance.__mro__:
+#                 return False
+#         else: # is an instance
+#             if Union in type(instance).__mro__:
+#                 return False
+
+#         return any(
+#             isinstance(instance, _type)
+#             for _type in cls._types
+#         )
+
+#     @classmethod
+#     def __subclasscheck__(cls, subclass):  # pylint: disable=unused-argument
+#         """
+#         @type: subclass: type
+#         @rtype: bool
+#         """
+
+
+#         print()
+#         print("cls:", type(cls), cls)
+#         print("subclass:", type(subclass), subclass)
+#         print()
+#         import pdb
+#         pdb.set_trace()
+#         print()
+        
+
+#         if not isinstance(subclass, type):
+#             raise TypeError(str.format(
+#                 "'subclass' must be a class."
+#             ))
+#         else:
+#             # ... prevent recursion
+#             if cls is Union:
+#                 if Union in subclass.__mro__:
+#                     return True  
+#             return any(
+#                 issubclass(subclass, _type)
+#                 for _type in cls._types
+#             )
+#Union._types = tuple()
+
+
+
+
+
+class Union_v1(TypeLogic):
     """
     Must be instanced before being used.
     """
@@ -122,3 +309,10 @@ class Tuple(TypeLogic):
         else:  # not a tuple
             return False
 
+
+def _hasattr(C, attr):
+    try:
+        return any(attr in B.__dict__ for B in C.__mro__)
+    except AttributeError:
+        # Old-style class
+        return hasattr(C, attr)
--- pyinterfaces/metatypes/test_pep484.py
+++ pyinterfaces/metatypes/test_pep484.py
@@ -3,11 +3,134 @@
 import unittest
 import collections
 
-from .pep484 import Union
+from .pep484 import Any, Union, UnionMeta
+
+class MyClass(object):
+    pass
+class MyTuple(tuple):
+    pass
 
 
-class ReverseTestCase(unittest.TestCase):
+class AnyTestCase(unittest.TestCase):
+    """
+    Unit-tests for the Any class.
+    """
+    def test_instancecheck(self):
+        self.assertTrue(isinstance(12, Any))
+        self.assertTrue(isinstance('ba', Any))
+        self.assertTrue(isinstance(['ba'], Any))
+        self.assertTrue(isinstance(None, Any))
+        self.assertTrue(isinstance(type(None), Any))
+        self.assertTrue(isinstance(map, Any))
+        self.assertTrue(isinstance(dict, Any))
+        self.assertTrue(isinstance(lambda: NotImplemented, Any))
+        self.assertTrue(isinstance(NotImplemented, Any))
+
+        self.assertTrue(isinstance(MyClass, Any))
+        self.assertTrue(isinstance(MyClass(), Any))
+        
+    def test_subclasscheck(self):
+        """Confirm that issubclass({{x}}, Any) works."""
+        self.assertTrue(issubclass(type(None), Any))
+        self.assertTrue(issubclass(dict, Any))
+        self.assertTrue(issubclass(MyClass, Any))
+
+        valid = [
+            MyClass(), 12, 'ba', ['ba'], tuple(), None, map,
+            lambda: NotImplemented, NotImplemented
+        ]
+        for obj in valid:
+            with self.assertRaises(TypeError):
+                issubclass(obj, Any)
+
+class TargetedTestCase(unittest.TestCase):
     def test_basic(self):
-        combined = Union(list, tuple)
-        self.assertTrue(issubclass(combined, collections.Sequence))
-        # Currently this errors because Union produces an instance, not a class
+        self.assertTrue(issubclass(Union(list), list))
+
+
+# class UnionTestCase(unittest.TestCase):
+#     def test_direct_class(self):
+#         combined = Union(list, tuple)
+
+#         # self.assertFalse(isinstance(combined, Union))
+#         self.assertTrue(issubclass(combined, Union))
+
+# #        self.assertFalse(isinstance(Union, combined))
+#         self.assertFalse(issubclass(Union, combined))
+
+#         # self.assertFalse(isinstance(Union, Union))
+#         self.assertTrue(issubclass(Union, Union))
+
+#         # self.assertFalse(isinstance(combined, combined))
+#         self.assertFalse(issubclass(combined, combined))
+
+#     def test_single_types(self):
+#         single = Union(list)
+#         self.assertTrue(issubclass(Union(list), list))
+#         self.assertTrue(issubclass(list, Union(list)))
+
+#         self.assertTrue(isinstance("", Union(str)))
+#         self.assertFalse({}, Union(tuple))
+#         with self.assertRaises(TypeError):
+#             isinstance(Union(str), "")
+
+#         self.assertFalse(isinstance(Union(str), str))
+#         self.assertFalse(isinstance(str, Union(str)))
+
+#     def test_empty(self):
+#         empty = Union()
+#         self.assertEqual(empty._types, tuple([Any]))
+#         self.assertTrue(isinstance([], empty))
+#         self.assertTrue(issubclass(type(None), empty))
+
+    # def test_self_reference(self):
+    #     combined = Union(Union, list)
+
+    #     self.assertTrue(issubclass(combined, Union))
+    #     self.assertTrue(issubclass(combined, combined))
+    #     self.assertTrue(issubclass(Union, combined))
+
+    #     self.assertFalse(isinstance(combined, Union))
+    #     self.assertFalse(isinstance(combined, combined))
+    #     self.assertFalse(isinstance(Union, combined))
+
+    # def test_isinstance(self):
+    #     combined = Union(list, tuple)
+
+    #     self.assertTrue(isinstance([], combined))
+    #     self.assertTrue(isinstance([12, 32], combined))
+    #     self.assertTrue(isinstance(tuple(), combined))
+    #     self.assertTrue(isinstance(MyTuple(), combined))
+
+    #     self.assertFalse(isinstance(tuple, combined))
+    #     self.assertFalse(isinstance(list, combined))
+    #     self.assertFalse(isinstance("asa", combined))
+    #     self.assertFalse(isinstance(str, combined))
+    #     self.assertFalse(isinstance(None, combined))
+
+    # def test_issubclass(self):
+    #     combined = Union(list, tuple)
+
+    #     self.assertTrue(issubclass(str, combined))
+    #     self.assertTrue(issubclass(list, combined))
+    #     self.assertTrue(issubclass(MyTuple, combined))
+
+    #     self.assertFalse(issubclass(None, combined))
+    #     self.assertFalse(issubclass(type(None), combined))
+    #     self.assertFalse(issubclass(MyClass, combined))
+    #     from collections import Sequence
+    #     self.assertFalse(issubclass(Sequence, combined))
+    #     self.assertFalse(issubclass(dict, combined))
+
+    #     with self.assertRaises(TypeError):
+    #         issubclass(MyClass(), combined)
+
+
+# class ReverseTestCase(unittest.TestCase):
+#     def test_basic(self):
+#         combined = Union(list, tuple)
+#         self.assertTrue(issubclass(combined, collections.Sequence))
+#         # Currently this errors because Union produces an instance, not a class
+
+if __name__ == "__main__":
+    unittest.main()
